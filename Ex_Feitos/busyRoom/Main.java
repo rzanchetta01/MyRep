8fb585601280282a66be26c756b53773a843d5af
rzanchetta01 <61316822+rzanchetta01@users.noreply.github.com>
2021-08-02 17:24:24 -0300

    busyRoom Exercise
    Primeiro uso de arays e ToString

---
+++ Ex_Feitos/busyRoom/Main.java
@@ -0,0 +1,40 @@
+package Ex_Feitos.busyRoom;
+/*package busyRoom;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+            
+        cadastro[] cadastro = new cadastro[11];
+
+        System.out.println("quantidade de estudantes");
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            System.out.println();
+            System.out.println("Rent #" + i + ":");
+            System.out.print("Name: ");
+            sc.nextLine();
+            String nome = sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.nextLine();
+            System.out.print("Room: ");
+            int room = sc.nextInt();
+            cadastro[room] = new cadastro(nome, email);
+
+        }
+        System.out.println();
+        System.out.println("Busy rooms:");
+        for (int i=0; i<10; i++) {
+            if (cadastro[i] != null) {
+            System.out.println(i + ": " + cadastro[i]);
+                
+            }  
+        }
+    sc.close(); 
+    }
+}
+    */
---
+++ Ex_Feitos/busyRoom/cadastro.java
@@ -0,0 +1,34 @@
+//package Ex_Feitos.busyRoom;
+/*package busyRoom;
+public class cadastro {
+    
+
+    private String nome;
+    private String email;
+
+    public cadastro(String nome, String email){
+        this.nome = nome;
+        this.email = email;
+    }
+
+    
+
+
+
+    public String getEmail() {
+        return email;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String toString(){
+        return this.nome + "," + this.email;
+    }
+
+}*/
