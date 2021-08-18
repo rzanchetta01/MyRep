a9256480490d5d8f72acc3a135980b48bd7dedfb
rzanchetta01 <61316822+rzanchetta01@users.noreply.github.com>
2021-08-18 14:59:46 -0300

    ex Test Files

---
+++ Ex_Feitos/exFiles/Main.java
@@ -0,0 +1,52 @@
+package Ex_Feitos.exFiles;
+
+/*import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+       List<Produto> lista = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        
+        int n = 1;
+
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("nome, quantidade, preco ");
+            String nome = sc.nextLine();
+            int quantidade = sc.nextInt();
+            double preco = sc.nextDouble();
+            sc.nextLine();
+            lista.add(new Produto(nome, quantidade, preco));
+        }
+        for (Produto produto : lista) {
+           System.out.println(produto.oConsole()); 
+        }
+
+
+
+
+ 
+        String path = "F:\\MyRep\\exFiles\\test.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+            
+
+            for (Produto produto : lista ){
+                bw.write(produto.oFile());
+                bw.newLine();
+            }
+
+            
+        } catch (IOException e) {
+           e.getMessage();
+        }
+        sc.close();
+    }
+}
+*/
---
+++ Ex_Feitos/exFiles/Produto.java
@@ -0,0 +1,48 @@
+package Ex_Feitos.exFiles;
+/*
+public class Produto {
+
+    private String nome;
+    private int quantidade;
+    private double preco;
+
+
+    public Produto(String nome, int quantidade, double preco) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return this.quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPreco() {
+        return this.preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+
+    public String oFile() {
+        return getNome() + ","+ getQuantidade()*getPreco();
+    }
+    public String oConsole() {
+        return getNome() + ","+ getQuantidade() + "," + getPreco();
+    }
+
+}*/
